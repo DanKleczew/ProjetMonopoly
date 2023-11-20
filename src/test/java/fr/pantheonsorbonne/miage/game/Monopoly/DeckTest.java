1174b47a933448dd3e6085ac5cacc29132504021
ZHENG Raymond <rerezheng@gmail.com>
2023-11-20 15:48:27 +0100

    Quelque petit changement pour plus avoir de jaune

--- src/test/java/fr/pantheonsorbonne/miage/game/Monopoly/DeckTest.java
+++ src/test/java/fr/pantheonsorbonne/miage/game/Monopoly/DeckTest.java
@@ -1,19 +1,15 @@
 package fr.pantheonsorbonne.miage.game.Monopoly;
+import fr.pantheonsorbonne.miage.game.Monopoly.Cards.*;
+//import fr.pantheonsorbonne.miage.game.Monopoly.Cases.*;
+//import fr.pantheonsorbonne.miage.game.Monopoly.Players.*;
+
 
 import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Queue;
 
-import fr.pantheonsorbonne.miage.game.Monopoly.Cards.Card;
-import fr.pantheonsorbonne.miage.game.Monopoly.Cards.CardDeplacement;
-import fr.pantheonsorbonne.miage.game.Monopoly.Cards.CardGoToPrison;
-import fr.pantheonsorbonne.miage.game.Monopoly.Cards.CardSteal;
-import fr.pantheonsorbonne.miage.game.Monopoly.Cards.CardWinLose;
-import fr.pantheonsorbonne.miage.game.Monopoly.Cards.Deck;
-import fr.pantheonsorbonne.miage.game.Monopoly.Cards.DeckCaisse;
+
 
 public abstract class DeckTest {
     protected Queue<Card> deck;
@@ -26,7 +22,7 @@ public abstract class DeckTest {
     // };
 
     public void piocher(){
-        Deck caisse = new DeckCaisse();
+        //Deck caisse = new DeckCaisse();
         //caisse
         // List<Card> DECK_ARRAY_CAISSE = new ArrayList<Card>(Arrays.asList(
         //     new CardDeplacement("Départ",0),
@@ -45,6 +41,7 @@ public abstract class DeckTest {
         //     new CardDeplacement("Gare la plus proche"),
         //     new CardWinLose(10),
         //     new CardWinLose(100))
+
         // );
         // Queue<Card> deck = new ArrayDeque<Card>();
         // deck.addAll(DECK_ARRAY_CAISSE);
@@ -97,8 +94,8 @@ public abstract class DeckTest {
 
 
     private void melangeDeck(List<Card> deckArray){
-        Collections.shuffle(deckArray);
-        this.deck = new ArrayDeque<Card>();
-        this.deck.addAll(deckArray);
+        // Collections.shuffle(deckArray);
+        // this.deck = new ArrayDeque<Card>();
+        // this.deck.addAll(deckArray);
     }
 }
