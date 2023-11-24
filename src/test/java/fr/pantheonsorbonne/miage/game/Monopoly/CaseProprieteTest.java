27d3b13fb8031a7e3e2b01469d9a0d040a411230
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2023-11-24 10:56:26 +0100

    un petit test

---
+++ src/test/java/fr/pantheonsorbonne/miage/game/Monopoly/CaseProprieteTest.java
@@ -0,0 +1,19 @@
+package fr.pantheonsorbonne.miage.game.Monopoly;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import fr.pantheonsorbonne.miage.game.Monopoly.Cases.*;
+
+public class CaseProprieteTest {
+    
+    @Test
+    public void setAsJailTest(){
+        CasePropriete a = new CasePropriete("Boulevard", 200 , TypePropriete.BLEU);
+
+        a.setAsJail(true);
+
+        assertEquals(true, a.isAJail());
+    }
+}
