181673bfd7b1fff45c175811d2e3940bb982687a
ZHENG Raymond <rerezheng@gmail.com>
2023-11-29 21:23:33 +0100

    before push

--- src/main/java/fr/pantheonsorbonne/miage/game/Monopoly/ToolBox.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Monopoly/ToolBox.java
@@ -7,68 +7,57 @@ import fr.pantheonsorbonne.miage.game.Monopoly.Cases.Case;
 import fr.pantheonsorbonne.miage.game.Monopoly.Cases.CaseAchetable;
 import fr.pantheonsorbonne.miage.game.Monopoly.Cases.CasePropriete;
 import fr.pantheonsorbonne.miage.game.Monopoly.Players.Dumb;
+import fr.pantheonsorbonne.miage.game.Monopoly.Players.IsBankruptException;
 import fr.pantheonsorbonne.miage.game.Monopoly.Players.Player;
 import fr.pantheonsorbonne.miage.game.Monopoly.Players.VoidBot;
 
-public class ToolBox  {
-    
+public class ToolBox {
+
     // public static Map<Integer, int[]> perfectBordToMap(String donne){
-        
-    //     Map<Integer, int[]> map = new Map<Integer,int[]>() {
-            
-    //     };
 
-    //     return map;
+    // Map<Integer, int[]> map = new Map<Integer,int[]>() {
+
+    // };
+
+    // return map;
     // }
-        public static PerfectBoard mapToPerfectBoard(Map<String,String> map){
-            Player toi = new VoidBot(0);
-            Player pasToi = new VoidBot(1);
-            PerfectBoard plateauTampon = new PerfectBoard();
-            List<CaseAchetable> listeDesProp = plateauTampon.allProprietes;
-            for (Integer i = 0; i < listeDesProp.size(); i++){
-                CaseAchetable currProp = listeDesProp.get(i);
-                String[] stringpetee = map.get(Integer.toString(i)).split(";");
-                switch (stringpetee[0]){
-                    case "1":
-                        currProp.setOwner(toi, true);
-                        break;
-                    case "2":
-                        currProp.setOwner(pasToi, true);
-                        break;
-                    default:
-                        break;
-                }
-
-
-                if (stringpetee[1].equals("6")){
-                    ((CasePropriete) currProp).setAsJail(true);
-                }
-                else{
-                    for (int j = 0; j<Integer.parseInt(stringpetee[1]); j++){
+    public static PerfectBoard mapToPerfectBoard(Map<String, String> map) throws IsBankruptException {
+        Player toi = new VoidBot(0);
+        Player pasToi = new VoidBot(1);
+        PerfectBoard plateauTampon = new PerfectBoard();
+        List<CaseAchetable> listeDesProp = plateauTampon.allProprietes;
+        for (Integer i = 0; i < listeDesProp.size(); i++) {
+            CaseAchetable currProp = listeDesProp.get(i);
+            String[] stringpetee = map.get(Integer.toString(i)).split(";");
+            switch (stringpetee[0]) {
+                case "1":
+                    currProp.setOwner(toi, true);
+                    break;
+                case "2":
+                    currProp.setOwner(pasToi, true);
+                    break;
+                default:
+                    break;
+            }
+            if (stringpetee[1].equals("6")) {
+                ((CasePropriete) currProp).setAsJail();
+            } else {
+                for (int j = 0; j < Integer.parseInt(stringpetee[1]); j++) {
                     ((CasePropriete) currProp).addHouse();
-                    }
                 }
-             }
-
-            // for (String indice : map.keySet()){
-            //     int indicePlateau = Integer.parseInt(indice);
-            //     String [] StringSplit = map.get(indice).split(";");
-
-            //     switch (StringSplit[0]) {
-            //         case "1":
-            //             if ()
-            //             break;
-                
-            //         default:
-            //             break;
-            //     }
-
-            //     if (Integer.parseInt(StringSplit[0])  ){
-
-            //     }
-            // }
-
-            return ;
+            }
         }
+        return null;
+    }
+
+    public static String perfectBoardToMap(PerfectBoard board) {
+
+        return null;
+    }
+
+    public static CasePropriete StringToCasePropriete(String body) {
+
+        return null;
+    }
 
 }
