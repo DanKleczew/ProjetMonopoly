c7b44bd144542b41f64244fbd2b4ed28adcb2ee7
ZHENG Raymond <rerezheng@gmail.com>
2023-11-30 16:22:28 +0100

    toolBoxp1

--- src/main/java/fr/pantheonsorbonne/miage/game/Monopoly/ToolBox.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Monopoly/ToolBox.java
@@ -1,10 +1,14 @@
 package fr.pantheonsorbonne.miage.game.Monopoly;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import fr.pantheonsorbonne.miage.game.Monopoly.Cases.Case;
 import fr.pantheonsorbonne.miage.game.Monopoly.Cases.CaseAchetable;
+import fr.pantheonsorbonne.miage.game.Monopoly.Cases.CaseGare;
 import fr.pantheonsorbonne.miage.game.Monopoly.Cases.CasePropriete;
 import fr.pantheonsorbonne.miage.game.Monopoly.Players.Dumb;
 import fr.pantheonsorbonne.miage.game.Monopoly.Players.IsBankruptException;
@@ -50,9 +54,58 @@ public class ToolBox {
         return null;
     }
 
-    public static String perfectBoardToMap(PerfectBoard board) {
+    public static Map<String,String> perfectBoardToMap(PerfectBoard board, Player player) {
+        Deque<Player> listeJoueurs = board.getListeJoueurs();
+        List<CaseAchetable> listeDesProp = board.allProprietes;
+        Map<String, String> map = new HashMap<String,String>();
+        int i = 0;
+        for (CaseAchetable proprieteParticuliere : listeDesProp ) {
+            StringBuilder builder = new StringBuilder();
+            if (proprieteParticuliere.getOwner().equals(player.getID())){
+                builder.append("1;");
+            }else{
+                builder.append("2;");
+            }
+            if(proprieteParticuliere instanceof CasePropriete){
+                CasePropriete propriete = (CasePropriete) proprieteParticuliere;
+                if(propriete.isAJail()){
+                    builder.append("6");
+                }
+                else{
+                builder.append(Integer.toString(propriete.getNombreMaisons()));
+                }
+            }
+            else if(proprieteParticuliere instanceof CaseGare){
+                CaseGare propriete = (CaseGare) proprieteParticuliere;
+                
+            }
 
-        return null;
+            map.put(Integer.toString(i), builder.toString());
+            i++;
+        }
+        return map;
+
+
+            // CaseAchetable currProp = listeDesProp.get(i);
+            // String[] stringpetee = map.get(Integer.toString(i)).split(";");
+            // switch (stringpetee[0]) {
+            //     case "1":
+            //         currProp.setOwner(toi, true);
+            //         break;
+            //     case "2":
+            //         currProp.setOwner(pasToi, true);
+            //         break;
+            //     default:
+            //         break;
+            // }
+            // if (stringpetee[1].equals("6")) {
+            //     ((CasePropriete) currProp).setAsJail();
+            // } else {
+            //     for (int j = 0; j < Integer.parseInt(stringpetee[1]); j++) {
+            //         ((CasePropriete) currProp).addHouse();
+            //     }
+            // }
+
     }
 
     public static CasePropriete StringToCasePropriete(String body) {
