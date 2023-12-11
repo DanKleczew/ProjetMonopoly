92264f21b5e1fbe58c922227fccad81175cb5984
Dan Kleczewski <Dan.Kleczewski1@etu.univ-paris1.fr>
2023-12-11 15:20:08 +0100

    commit quasi final

--- src/main/java/fr/pantheonsorbonne/miage/game/Monopoly/LocalMonopolyApp.java
+++ src/main/java/fr/pantheonsorbonne/miage/game/Monopoly/LocalMonopolyApp.java
@@ -12,23 +12,11 @@ public final class LocalMonopolyApp extends MonopolyEngine {
     }
 
     public static void main(String... args) throws IsBankruptException {
-        int winner1 = 0;
-        int winner2 = 0;
 
-
-        for  (int i = 0; i < 2000; i++){
-        PerfectBoard plateauEnLocal = new PerfectBoard(new CleverBot(0), new CleverBot(1) , new VoidBot(2), new VoidBot(3));
-        LocalMonopolyApp localMonopoly = new LocalMonopolyApp(plateauEnLocal);
+        PerfectBoard plateauEnLocal = new PerfectBoard(new CleverBot(0), new CleverBot(1) , new VoidBot(2), new Dumb(3));
+        MonopolyEngine localMonopoly = new LocalMonopolyApp(plateauEnLocal);
         
-        if(localMonopoly.play() <=1){
-            winner1++;
-        }
-        else{
-            winner2++;
-        }
-        }
-        System.out.println(winner1);
-        System.out.println(winner2);
+        localMonopoly.play();
         System.exit(0);
 
     }
